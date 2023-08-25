322bbeca47ad55113c237fa683314c050382c18c
alyss01 <alys.baker@ten10.com>
2023-08-25 14:11:40 +0100

    finished

--- src/test/java/StepDefinitions/RegisterPageStepdefs.java
+++ src/test/java/StepDefinitions/RegisterPageStepdefs.java
@@ -1,11 +1,25 @@
 package StepDefinitions;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import pages.ProductsPage;
+
+import static StepDefinitions.Hooks.driver;
+import static StepDefinitions.Hooks.wait;
 
 
 public class RegisterPageStepdefs {
 
+
     @And("I will be directed to the registration page")
     public void directToRegisterPage() {
     }
-
+    @Then("I am registered")
+    public void iAmRegistered() {
+        WebElement yourAccount = driver.findElement(By.cssSelector("h1"));
+        String expected = "POPULAR PRODUCTS";
+        Assert.assertEquals(expected, yourAccount.getText());
+    }
 }
--- src/test/resources/features/Register.feature
+++ src/test/resources/features/Register.feature
@@ -5,4 +5,5 @@ Feature: Registering a user
     Given I am on the Sign in page
     When I press "No account?Create one here"
     And I will be directed to the registration page
+    Then I am Registered
 
